bc81c23f0d4724d89250a57aa4667af818184452
Fabian Salge <fabiansalge02@gmail.com>
2025-11-23 16:45:06 +0000

    update leaderboard

--- src/data/coffeeLeaderboard.ts
+++ src/data/coffeeLeaderboard.ts
@@ -11,15 +11,24 @@ export interface CoffeeShop {
 export const coffeeShops: CoffeeShop[] = [
   {
     rank: 1,
+    name: 'La Bodega',
+    location: 'Bellgrove Street',
+    rating: 4.8,
+    visitDate: 'Nov 2025',
+    notes: 'This place is amazing. Coffee, food, and hot sauce.',
+    highlight: '👑'
+  },
+  {
+    rank: 2,
     name: 'Kaffateria',
     location: 'Dundas Street',
     rating: 4.7,
     visitDate: 'Sep 2025',
     notes: 'Great location, Excellent coffee. Almost perfect!!',
-    highlight: '👑'
+    highlight: '👑 - very close second'
   },
   {
-    rank: 2,
+    rank: 3,
     name: 'Laboratorio Espresso',
     location: 'West Nile Street',
     rating: 4.6,
@@ -27,7 +36,7 @@ export const coffeeShops: CoffeeShop[] = [
     notes: 'Very good coffee and no line at all. Super quick and delicious.',
   },
   {
-    rank: 3,
+    rank: 4,
     name: 'Gordon Street Coffee',
     location: 'Gordon St (as you might\'ve guessed)',
     rating: 4.3,
@@ -35,7 +44,7 @@ export const coffeeShops: CoffeeShop[] = [
     notes: 'Great coffee. House blend is awesome.',
   },
   {
-    rank: 4,
+    rank: 5,
     name: 'Through the House',
     location: 'High St',
     rating: 4.1,
@@ -43,7 +52,7 @@ export const coffeeShops: CoffeeShop[] = [
     notes: 'Cool spot, good coffee, free sparkling water!!',
   },
   {
-    rank: 5,
+    rank: 6,
     name: 'Kloud',
     location: 'Hutcheson Steet',
     rating: 3.9,
@@ -51,7 +60,15 @@ export const coffeeShops: CoffeeShop[] = [
     notes: 'First time was surpirsingly good. Second visit, was a burnt coffee with long wait time.',
   },
   {
-    rank: 6,
+    rank: 7,
+    name: 'Artrez',
+    location: 'Saltmarket',
+    rating: 3.4,
+    visitDate: 'Nov 2025',
+    notes: 'Beans were a bit too sweet for my taste.',
+  }
+  {
+    rank: 8,
     name: 'Blank Street',
     location: 'Gordon Street',
     rating: 3.4,
